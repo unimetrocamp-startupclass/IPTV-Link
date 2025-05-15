1c580d5a3ea327f50ea44dca09744417ddf1ca3c
Pedro Maia <115093035+pedromaia36602@users.noreply.github.com>
2025-05-15 15:51:09 -0300

    Delete Up_Gestor_de_clientes-main directory

--- Up_Gestor_de_clientes-main/Up_Gestor_de_clientes-main/Gestao_de_clientes/db.py
+++
@@ -1,5 +0,0 @@
-# db.py
-import sqlite3
-
-def conectar():
-    return sqlite3.connect("usuarios.db")
--- Up_Gestor_de_clientes-main/Up_Gestor_de_clientes-main/Gestao_de_clientes/main_cadastro.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-import sys
-from cadastro_widget import CadastroWindow
-
-app = QApplication(sys.argv)
-janela = CadastroWindow()
-janela.show()
-sys.exit(app.exec())
